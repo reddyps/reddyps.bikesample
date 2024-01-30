995af01948209d536f37a05996df411a275e4d92
50072581 <naveenkumar45@ltfs.com>
2024-01-30 10:27:16 +0530

    version 3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,11 +25,11 @@ const RESOURCES = {"assets/AssetManifest.bin": "27719fa4f55e84f48654ad6a87f74aa6
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "a44e396a0b43ebebb3519d4ff14dc8c1",
-"/": "a44e396a0b43ebebb3519d4ff14dc8c1",
-"main.dart.js": "344d9b7c55b6ffa63f61bcf617f53ff5",
+"index.html": "e7a789a663b2554d61b3dd19669c27c9",
+"/": "e7a789a663b2554d61b3dd19669c27c9",
+"main.dart.js": "172acf4ba309363d2797907c8e571bf2",
 "manifest.json": "5e39db19f33a1058eeaa5d62a303d476",
-"version.json": "673c3458469a088a1719b7da2b21ee71"};
+"version.json": "80c4e3cdbaedd94f72c8aa6d75175d9c"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
