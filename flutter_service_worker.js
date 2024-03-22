2fd9debf4891e60d35ab29193b941c547dec96be
50072581 <naveenkumar45@ltfs.com>
2024-03-22 11:04:55 +0530

    6

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -10,7 +10,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "1d46cb4031cff6bc03b96420ee25338c
 "assets/assets/LTFS-Logo.png": "96b01fcff53fd89dc93e60d9560adb40",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "5a80b3daab09905b9c8972887c57aecc",
-"assets/NOTICES": "7590bf6044577f4eeb3fa46a24ed9ca0",
+"assets/NOTICES": "567d50ee4c2135b9f9f9a43079ddee07",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
 "assets/shaders/ink_sparkle.frag": "4096b5150bac93c41cbc9b45276bd90f",
 "canvaskit/canvaskit.js": "eb8797020acdbdf96a12fb0405582c1b",
@@ -26,14 +26,17 @@ const RESOURCES = {"assets/AssetManifest.bin": "1d46cb4031cff6bc03b96420ee25338c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "8b5ec8a8270abe1a0802ea40d4160a7e",
-"/": "8b5ec8a8270abe1a0802ea40d4160a7e",
-"main.dart.js": "48dfb2a61ca958cb8c5620140da6ec60",
+"icons/loader.gif": "5dcd8da34a28b83c6db998a2fce31455",
+"icons/LTFS-Logo.png": "96b01fcff53fd89dc93e60d9560adb40",
+"index.html": "214e3f98a5e4ff66702b54ae2c4b37cf",
+"/": "214e3f98a5e4ff66702b54ae2c4b37cf",
+"main.dart.js": "96a79219288d3ccc760156ec6ee5d663",
 "manifest.json": "5e39db19f33a1058eeaa5d62a303d476",
 "phonepe/package.json": "fbca758ac25861a0fd21403eb2cb980c",
 "phonepe/phonepesdk.js": "9273d7f48973358f8d872f90b2056984",
 "phonepe/README.md": "0159003e35131f5848d2215aecd9c398",
-"version.json": "9f161016fbb60b5bad9bcfa042dba600"};
+"styles.css": "b536a4baa886de9679149b653bdaebf5",
+"version.json": "d1e29c3368e19e69802310dc1d76ee60"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
